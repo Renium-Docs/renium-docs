25a73f2855d3e903cb63498ff981f633ef654284
Zeynep <zeynepcaglabaykan@outlook.com>
2025-11-20 14:23:02 +0300

    update docusaurus dependencies to version 3.9.2

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -31,11 +31,11 @@ const config: Config = {
           routeBasePath: 'docs',
           sidebarPath: './sidebars.ts',
           editUrl: ({locale, docPath}) => {
-          if (locale === 'en') {
-            return `https://github.com/Renium-Docs/renium-docs/edit/main/i18n/${locale}/docusaurus-plugin-content-docs-public/current/${docPath}`;
-          }
-          return `https://github.com/Renium-Docs/renium-docs/edit/main/docs/common/${docPath}`;
-        },
+            if (locale === 'en') {
+              return `https://github.com/Renium-Docs/renium-docs/edit/main/i18n/${locale}/docusaurus-plugin-content-docs-public/current/${docPath}`;
+            }
+            return `https://github.com/Renium-Docs/renium-docs/edit/main/docs/common/${docPath}`;
+          },
         },
         blog: false,
         sitemap: {
@@ -48,38 +48,38 @@ const config: Config = {
     ],
   ],
 
-plugins: [
-  [
-    '@docusaurus/plugin-content-docs',
-    {
-      id: 'cfdbfc79-7598-4828-6b2a-08dc27e3fe33',
-      path: 'docs/merged/cfdbfc79-7598-4828-6b2a-08dc27e3fe33',
-      routeBasePath: 'cfdbfc79-7598-4828-6b2a-08dc27e3fe33/docs',
-      sidebarPath: './sidebars.cfdbfc79-7598-4828-6b2a-08dc27e3fe33.ts',
-      
-      editUrl: ({locale, docPath}) => {
-        if (locale === 'en') {
-          return `https://github.com/Renium-Docs/renium-docs/edit/main/i18n/${locale}/docusaurus-plugin-content-docs-cfdbfc79-7598-4828-6b2a-08dc27e3fe33/current/${docPath}`;
-        }
+  plugins: [
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'cfdbfc79-7598-4828-6b2a-08dc27e3fe33',
+        path: 'docs/merged/cfdbfc79-7598-4828-6b2a-08dc27e3fe33',
+        routeBasePath: 'cfdbfc79-7598-4828-6b2a-08dc27e3fe33/docs',
+        sidebarPath: './sidebars.cfdbfc79-7598-4828-6b2a-08dc27e3fe33.ts',
         
-        const fs = require('fs');
-        const path = require('path');
+        editUrl: ({locale, docPath}) => {
+          if (locale === 'en') {
+            return `https://github.com/Renium-Docs/renium-docs/edit/main/i18n/${locale}/docusaurus-plugin-content-docs-cfdbfc79-7598-4828-6b2a-08dc27e3fe33/current/${docPath}`;
+          }
+          
+          const fs = require('fs');
+          const path = require('path');
+          
+          const companySpecificPath = path.join(
+            __dirname,
+            'docs/companies/cfdbfc79-7598-4828-6b2a-08dc27e3fe33',
+            docPath
+          );
+          
+          if (fs.existsSync(companySpecificPath)) {
+            return `https://github.com/Renium-Docs/renium-docs/edit/main/docs/companies/cfdbfc79-7598-4828-6b2a-08dc27e3fe33/${docPath}`;
+          }
         
-        const companySpecificPath = path.join(
-          __dirname,
-          'docs/companies/cfdbfc79-7598-4828-6b2a-08dc27e3fe33',
-          docPath
-        );
-        
-        if (fs.existsSync(companySpecificPath)) {
-          return `https://github.com/Renium-Docs/renium-docs/edit/main/docs/companies/cfdbfc79-7598-4828-6b2a-08dc27e3fe33/${docPath}`;
-        }
-      
-        return `https://github.com/Renium-Docs/renium-docs/edit/main/docs/common/${docPath}`;
+          return `https://github.com/Renium-Docs/renium-docs/edit/main/docs/common/${docPath}`;
+        },
       },
-    },
+    ],
   ],
-],
 
   themeConfig: {
     image: 'img/docusaurus-social-card.jpg',
@@ -90,15 +90,15 @@ plugins: [
         src: 'img/favicon.ico',
       },
       items: [
-          {
-            to: '/docs/intro',  
-            position: 'left',
-            label: 'Dokümantasyon',
-          },
-          {
-            type: 'localeDropdown',
-            position: 'right',
-          },
+        {
+          to: '/docs/intro',  
+          position: 'left',
+          label: 'Dokümantasyon',
+        },
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
         {
           href: 'https://github.com/Renium-Docs/renium-docs',
           label: 'GitHub',
@@ -111,14 +111,6 @@ plugins: [
       apiKey: 'adc4592bd9db05b55df4244a3ca8a735',
       indexName: 'Renium Docs',
       contextualSearch: true,
-      externalUrlRegex: 'external\\.com|domain\\.com',
-      replaceSearchResultPathname: {
-        from: '/docs/',
-        to: '/',
-      },
-      searchParameters: {
-        facetFilters:['language:LANGUAGE'],
-      },
       searchPagePath: 'search',
     },
     footer: {
